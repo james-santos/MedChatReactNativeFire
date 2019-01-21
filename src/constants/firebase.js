542484fc43f43c94555b57840102c2f509448c53
James <james.chris.santos@gmail.com>
2019-01-20 23:31:25 -0800

    fix: api keys added to src / constants / firebase.js

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: 'AIzaSyCrvnXCuRxUH-nLKMtVyhIMYoDTOGHUIaY',
+  authDomain: 'medchat-react-native-firebase.firebaseapp.com',
+  databaseURL: 'https://medchat-react-native-firebase.firebaseio.com',
+  projectId: 'medchat-react-native-firebase',
+  storageBucket: 'medchat-react-native-firebase.appspot.com',
+  messagingSenderId: '186291366015',
 };
